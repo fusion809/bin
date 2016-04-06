e242dfac952f206073e0db543c7b2a59df94bbe9
fusion809 <brentonhorne77@gmail.com>
2016-04-06 13:35:06 +1000

    Updating scripts

--- quick-install.sh
+++ quick-install.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
 git clone https://github.com/fusion809/bin $HOME/bin
 PT=$(echo $PATH | grep "$HOME/bin")
 if ! [[ -n $PT ]]; then
-  if [[ $SHELL == "/bin/bash" ]]; then
+  SH=${SHELL##*/}
+  if [[ $SH == "bash" ]]; then
     printf 'export PATH=$PATH:$HOME/bin' >> $HOME/.bashrc
     source $HOME/.bashrc
-  elif [[ $SHELL == "/bin/zsh" ]]; then
+  elif [[ $SH == "zsh" ]]; then
     printf 'export PATH=$PATH:$HOME/bin' >> $HOME/.zshrc
     source $HOME/.zshrc
   fi
