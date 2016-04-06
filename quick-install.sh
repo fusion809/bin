77d57205d18cce8f9ff1b6e4354051777a35fd9c
fusion809 <brentonhorne77@gmail.com>
2016-04-06 13:39:05 +1000

    Updating scripts

--- quick-install.sh
+++ quick-install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-if ! [[ -d "$HOME/bin" ]]; then
+if [[ -d "$HOME/bin" ]]; then
+  printf "Ah, you've already got a ~/bin directory! \nWhere do you want to clone this repo to?"
+  read BIN
+  git clone https://github.com/fusion809/bin $BIN
+else
   git clone https://github.com/fusion809/bin $HOME/bin
 fi
 PT=$(echo $PATH | grep "$HOME/bin")
